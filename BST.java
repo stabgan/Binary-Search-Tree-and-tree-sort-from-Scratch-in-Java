1fd46e7668fdaccbf0089994273c579697d3b3ad
Kaustabh Ganguly <stabgan@users.noreply.github.com>
2026-03-14 16:21:56 +0530

    Revert "fix: modernize code + rewrite README"
    This reverts commit 55456a1ff99368ee645ea5139a229d754971c2ce.

--- BST.java
+++ BST.java
@@ -1,129 +1,147 @@
-class Node {
+class Node{
     int value;
     Node left;
     Node right;
 
-    Node(int data) {
-        this.value = data;
-        this.left = null;
-        this.right = null;
+    Node(int data){
+      this.value = data;
+      this.left = null;
+      this.right = null;
     }
+}
+
+class BST {
+
+    Node root;
+
+    public void insert(int data){
+      root = update(root,data);
+    }
+
+    public Node update(Node root, int data){
+      var temp = new Node(data);
+      if (root == null){
+        root = temp;
+      }
+      else {
+        if( data < root.value){
+          root.left = update(root.left,data);
+        }
+        else if ( data > root.value){
+          root.right = update(root.right,data);
+        }
+      }
+
+      return root;
+    }
+
+    public void sort(){
+      if (root != null){
+        inorder(root);
+      }
+    }
+
+    public void inorder(Node root){
+        if(root != null){
+          inorder(root.left);
+          System.out.println(root.value);
+          inorder(root.right);
+        }
+      }
+    
+    public void post(){
+      if (root != null){
+        postorder(root);
+      }
+    }
+
+    public void postorder(Node root){
+        if(root != null){
+          inorder(root.left);
+          inorder(root.right);
+          System.out.println(root.value);
+        }
+      }
+
+    public void contains(int data){
+      System.out.println(searchhelper(root, data) != null);
+    }
+
+
+    public Node searchhelper(Node root, int data){
+      if(root == null || root.value == data){
+        return root;
+      }
+      else {
+        if (data < root.value ){
+        return searchhelper(root.left , data);
+        }
+        else {
+        return searchhelper(root.right, data);
+        }
+      }
+    }
+
+    public void clear(){
+      root = null;
+    }
+
+    public void remove(int data){
+      root = _delete(root , data);
+      }
+    
+    private int _min(Node root) 
+    { 
+        int minv = root.value; 
+        while (root.left != null) 
+        { 
+          root = root.left; 
+        } 
+        minv = root.value; 
+        return minv; 
+    } 
+    
+    public Node _delete(Node root ,int data){
+      if (root == null)  return root; 
+      if (data < root.value ){
+        root.left =  _delete(root.left , data);
+        }
+      else if(data > root.value){
+        root.right = _delete(root.right, data);
+        }
+      else {
+        if (root.left == null) 
+            return root.right; 
+        else if (root.right == null) 
+            return root.left; 
+
+        root.value = _min(root.right); 
+        root.right = _delete(root.right, root.value); 
+      }
+
+      return root;
+    }
+    
 }
 
 public class BST {
 
-    Node root;
-
-    public void insert(int data) {
-        root = insertNode(root, data);
-    }
-
-    private Node insertNode(Node node, int data) {
-        if (node == null) {
-            return new Node(data);
-        }
-        if (data < node.value) {
-            node.left = insertNode(node.left, data);
-        } else if (data > node.value) {
-            node.right = insertNode(node.right, data);
-        }
-        return node;
-    }
-
-    public void sort() {
-        if (root != null) {
-            inorder(root);
-        }
-    }
-
-    public void inorder(Node node) {
-        if (node != null) {
-            inorder(node.left);
-            System.out.println(node.value);
-            inorder(node.right);
-        }
-    }
-
-    public void post() {
-        if (root != null) {
-            postorder(root);
-        }
-    }
-
-    public void postorder(Node node) {
-        if (node != null) {
-            postorder(node.left);
-            postorder(node.right);
-            System.out.println(node.value);
-        }
-    }
-
-    public boolean contains(int data) {
-        return search(root, data) != null;
-    }
-
-    private Node search(Node node, int data) {
-        if (node == null || node.value == data) {
-            return node;
-        }
-        if (data < node.value) {
-            return search(node.left, data);
-        }
-        return search(node.right, data);
-    }
-
-    public void clear() {
-        root = null;
-    }
-
-    public void remove(int data) {
-        root = deleteNode(root, data);
-    }
-
-    private int findMin(Node node) {
-        while (node.left != null) {
-            node = node.left;
-        }
-        return node.value;
-    }
-
-    private Node deleteNode(Node node, int data) {
-        if (node == null) return node;
-
-        if (data < node.value) {
-            node.left = deleteNode(node.left, data);
-        } else if (data > node.value) {
-            node.right = deleteNode(node.right, data);
-        } else {
-            if (node.left == null) return node.right;
-            if (node.right == null) return node.left;
-
-            node.value = findMin(node.right);
-            node.right = deleteNode(node.right, node.value);
-        }
-        return node;
-    }
-
-    public static void main(String[] args) {
-        BST tree = new BST();
-        tree.insert(50);
-        tree.insert(23);
-        tree.insert(55);
-        tree.insert(22);
-        tree.insert(11);
-        tree.insert(401);
-
-        System.out.println("Tree Sort (Inorder)");
-        tree.sort();
-
-        System.out.println("\nPostorder");
-        tree.post();
-
-        System.out.println("\nContains 11: " + tree.contains(11));
-        System.out.println("Contains 11111: " + tree.contains(11111));
-
-        tree.remove(23);
-        System.out.println("\nTree Sort after removing 23");
-        tree.sort();
-    }
+  public static void main(String[] args) {
+    BST tree = new BST();
+    tree.insert(50);
+    tree.insert(23);
+    tree.insert(55);
+    tree.insert(22);
+    tree.insert(11);
+    tree.insert(401);
+    System.out.println("\nTree Sort\n");
+    tree.sort();
+    System.out.println("\nPostorder\n");
+    tree.post();
+    tree.contains(11);
+    tree.contains(11111);
+    tree.remove(23);
+    System.out.println("\nTree Sort\n");
+    tree.sort();
+  }
 }
